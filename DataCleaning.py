164eed5f50dfc13336a3f185dfab7842c189ade7
Sean Leader <spleader13@gmail.com>
2022-10-03 13:55:48 -0700

    Added DataCleaning.py
    This file takes in one of the reformatted files, and spreads out the year ranges into individual rows correspond to that year.

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,1 +1,28 @@
 import pandas as pd
+
+df = pd.read_csv("RawData/AgeBySex21Reformatted.csv", encoding = "utf-8")
+
+#df['Y'] = df['County'].apply(lambda x : x.rsplit(' ', 1)[0])
+year_ranges = list(df['Year'].value_counts().index)
+
+year_range_dict = {}
+for year_range in year_ranges:
+    year1 = year_range.split("-")[0]
+    year2 = year_range.split("-")[1]
+    year_range_dict[year_range] = range(int(year1), int(year2) + 1)
+
+new_df = pd.DataFrame()
+
+for index, row in df.iterrows():
+
+    for year in year_range_dict[row['Year']]:
+
+        row['Year'] = year
+        new_df = pd.concat([new_df, row], axis = 1)
+
+new_df.transpose().to_csv("RawData/AgeBySex21Final.csv", index = False)
+#df.to_csv("RawData/AnnualPopulationEstimatesReformatted.csv", index = False)
+
+if __name__ == "__main__":
+
+    print(new_df.transpose())
